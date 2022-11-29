04873d1f39664abdc9465ddb60f7755e36196ca1
Rajini Preetha JOHN <rajinipreethaj@hotmail.com>
2022-11-29 11:54:10 +0800

    Edited py file

--- MyApp.py
+++ MyApp.py
@@ -16,7 +16,7 @@ def lottieurl_load(url):
 
 def local_css(file_name):
     with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>")
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     
 local_css('style/style.css')
 
